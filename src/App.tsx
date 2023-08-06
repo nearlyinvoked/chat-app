cf6eeef39cbb97d3ff8d296d0cd06d66552d172f
Kevin Reynaldo Laurens <kevin.laurens@indicocompany.com>
2023-08-06 22:23:46 +0700

    Kevin | Chat UI functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,63 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+
+type Chat = {
+  id: number
+  chat: string
+  from: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [chat, setChat] = useState<Chat[]>([])
+  const [chatInput, setChatInput] = useState<string>('')
+
+  const handleAddChat = (newChat: Chat) => {
+    if (newChat.chat === '') return
+    const reversedChat = [...chat].reverse()
+    const newArr = [...reversedChat, newChat]
+    setChat(newArr.reverse())
+    setChatInput('')
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container mx-auto">
+        <div className="flex flex-col min-h-screen py-5">
+          <div className="flex flex-col grow">
+            <div className="flex-1">
+              <div className="flex flex-col-reverse">
+                {chat.map(chat => (
+                  <div className="flex justify-end my-2">
+                    <div className="bg-blue-500 p-2 rounded-md text-white">
+                      <p>{chat.chat}</p>
+                      <div className="flex justify-end m-1">
+                        <p className='text-xs'>{chat.from}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          
+          <div className="flex flex-none">
+            <input className='p-2 rounded-md w-full ' type="text" placeholder='Enter a chat' value={chatInput} onChange={e => setChatInput(e.target.value)} onKeyDown={e => {
+              if (e.key === 'Enter') {
+                handleAddChat({
+                  id: Math.floor(Math.random() * 1000),
+                  chat: chatInput,
+                  from: 'Kevin'
+                })
+              }
+            }} />
+            <button className='hover:border-white mx-1' onClick={() => handleAddChat({
+              id: Math.floor(Math.random() * 1000),
+              chat: chatInput,
+              from: 'Kevin'
+            })}>Send</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
